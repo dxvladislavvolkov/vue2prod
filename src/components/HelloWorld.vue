db9dd74abfd49d7d0eef411a6abd20015ad0a5d2
dxvladislavvolkov <vladislavvolkov@vladislavs-mbp.corp.devexpress.com>
2021-12-17 15:35:53 +0300

    add devextreme

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,69 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form v-on:submit.prevent="onFormSubmit">
+      <dx-form
+        ref="form"
+        label-location="top"
+        :form-data="state"
+        :show-colon-after-label="false"
+      >
+        <dx-group-item caption="Sign In">
+          <dx-simple-item
+            data-field="firstName"
+            editor-type="dxTextBox"
+            :editor-options="{ readOnly: readonly }"
+          >
+            <dx-label text="First Name" />
+            <dx-required-rule v-if="!readonly" />
+          </dx-simple-item>
+        </dx-group-item>
+
+        <dx-button-item horizontal-alignment="center">
+          <dx-button-options
+            text="save"
+            type="success"
+            :use-submit-behavior="true"
+          />
+        </dx-button-item>
+      </dx-form>
+    </form>
   </div>
 </template>
 
 <script>
+import {
+  DxForm,
+  DxSimpleItem,
+  DxGroupItem,
+  DxButtonItem,
+  DxButtonOptions,
+  DxRequiredRule,
+  DxLabel,
+} from "devextreme-vue/form";
 export default {
   name: 'HelloWorld',
+  components: {
+    DxForm,
+  DxSimpleItem,
+  DxGroupItem,
+  DxButtonItem,
+  DxButtonOptions,
+  DxRequiredRule,
+  DxLabel,
+  },
+  data() {
+    return {
+      state: {}
+    }
+  },
   props: {
-    msg: String
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+  },
+  methods: {
+    onFormSubmit() {}
   }
 }
 </script>
